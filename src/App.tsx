6777d3942c712e3c90bb018354be02cb3bd11286
Aleksander <sanyok.10rus@gmail.com>
2022-08-30 16:32:51 +0300

    added one example

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,27 @@ function App() {
           isByContent={true}
           isLooped={true}
         >
-
         </FlexibleSlider>
-        
+        <FlexibleSlider
+          isLooped={false}  
+          isHaveDots={true}
+          // customArrowButtonRight={<div style={{width: "50px", height: "50px", backgroundColor: "aqua", borderRadius: "50%", margin: "0"}}></div>} 
+          // customDotsButtton={<div style={{width: "30px", height: "30px", backgroundColor: "aqua", borderRadius: "50%", marginTop: "-30px"}}></div>}
+          isArrowButtonsOnHover={false}
+          isDotsButtonsOnHover={false}
+          isByContent={false}
+        >
+          <div style={{width: "200px", height: "50px", backgroundColor: "aqua"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "gray"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "aqua"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "gray"}}></div>
+          <div style={{width: "200px", height: "50px", backgroundColor: "aqua"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "gray"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "aqua"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "gray"}}></div>
+          <div style={{width: "200px", height: "50px", backgroundColor: "aqua"}}></div>
+          <div style={{width: "200px", height: "50px",  backgroundColor: "gray"}}></div>
+        </FlexibleSlider>
       </div>
     </div>
   );
