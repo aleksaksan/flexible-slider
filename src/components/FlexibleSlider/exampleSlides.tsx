fc2212599baf93524ba7b1f6dda0482f1d9a0b1b
Aleksander <sanyok.10rus@gmail.com>
2022-08-30 16:22:09 +0300

    created slider v 1.0

---
+++ src/components/FlexibleSlider/exampleSlides.tsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './exampleSlides.css';
+
+export const exampleSlides: JSX.Element[] = [
+  <img src={logo} className="App-logo" alt="logo" />,
+  <img src={logo} className="App-logo" alt="logo" />,
+  <img src={logo} className="App-logo" alt="logo" />,
+  <img src={logo} className="App-logo" alt="logo" />,
+  <img src={logo} className="App-logo" alt="logo" />,
+  <img src={logo} className="App-logo" alt="logo" />,
+]
